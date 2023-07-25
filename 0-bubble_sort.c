9181e3b94a5c39aa400e6e307c09f8f759897377
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-25 19:05:04 +0000

    Task 0 - added early exit optimization

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,9 +10,11 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp;
 	size_t i, j;
+	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,8 +22,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -34,5 +34,7 @@ void cocktail_sort_list(listint_t **list);
 void switch_nodes(listint_t **list, listint_t *n1, listint_t *n2);
 void cocktail_shaker_sort(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge(int *array, size_t size, int *left, size_t left_size, int *right, size_t right_size);
+void merge_sort(int *array, size_t size);
 
 #endif
